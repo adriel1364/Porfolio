04cd5daf1711f26e1ba8742ce182926903a3c5fd
adriel_delosanto <adridelosanto@gmail.com>
2026-03-27 18:06:49 -0300

    porfolio personal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'practica' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('practica');
+    expect(app.titulo).toEqual('practica');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importante para futuras directivas
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule], // Añadimos esto por buena práctica
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'practica';
+  // Aquí definimos tus datos para que sea fácil cambiarlos luego
+  nombre: string = 'Adriel Delosanto';
+  titulo: string = 'Desarrollador Full Stack';
+  pasion: string = 'Desarrollador Full Stack con mentalidad de resolución de problemas. Me especializo en crear aplicaciones web robustas y escalables, uniendo un backend eficiente con experiencias de usuario intuitivas y modernas. Mi objetivo es transformar ideas complejas en productos digitales funcionales y de alto impacto.';
 }
